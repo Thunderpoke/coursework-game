db5dbce986064a3ef10e307940b7c095439ea287
Thunderpoke <58555036+Thunderpoke@users.noreply.github.com>
2020-01-30 23:49:24 +0000

    level 1 complete

---
+++ coursework-game/Assets/Teleport.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Teleport : MonoBehaviour
+{
+
+    public GameObject Portal;
+    public GameObject Player;
+
+    void onTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            StartCoroutine(Warp());
+            Debug.Log("Test");
+        }
+    }
+    IEnumerator Warp()
+    {
+        yield return new WaitForSeconds(1);
+        Player.transform.position = new Vector2 (Portal.transform.position.x, Portal.transform.position.y);
+    }
+
+
+}
